61e898e54301fd3c25dc67efb1812141bc8681d4
Алексей Нефф <neff.alexey@gmail.com>
2024-07-15 17:26:24 +0300

    1

--- lib/src/widgets/message_row/default_message_text.dart
+++ lib/src/widgets/message_row/default_message_text.dart
@@ -113,7 +113,6 @@ class DefaultMessageText extends StatelessWidget {
           messageOptions.currentUserTextColor(context) :
           messageOptions.textColor,
         textAlign: isOwnMessage ? TextAlign.right : TextAlign.left,
-        display: Display.inline
       );
       return Html(
         data: '<div class="message-body">$text</div>',
